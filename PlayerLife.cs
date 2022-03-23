9d03d148610059fba8eaa5bf4e92690e109b5924
Pedro Knevitz <102121819+Knevitzzzz@users.noreply.github.com>
2022-03-23 00:49:49 -0300

    Update PlayerLife.cs

--- PlayerLife.cs
+++ PlayerLife.cs
@@ -16,6 +16,7 @@ public class PlayerLife : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      //Busca O Animtaor Anexado Ao Player Com Este Script
       anim = GetComponent<Animator>();
     }
 
